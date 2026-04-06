f406a183b157e5899037be3cf15d1d98d7464657
Glenn Bolton <glenn@defendable.design>
2026-04-06 17:04:23 +1000

    Add type exports for AdminPageLayout and DefaultPageLayout components

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,6 +1,8 @@
 export { AdminPageLayout } from "./AdminPageLayout";
+export type { AdminPageLayoutProps } from "./AdminPageLayout";
 export { ColumnRail, getPersistedWidth, getPersistedCollapsedState, persistCollapsedState } from "./ColumnRail";
 export type { ColumnRailProps } from "./ColumnRail";
 export { DefaultPageLayout } from "./DefaultPageLayout";
+export type { DefaultPageLayoutProps } from "./DefaultPageLayout";
 export { ThreeColumnLayout } from "./ThreeColumnLayout";
 export type { Breakpoint, ColumnConfig, ThreeColumnLayoutProps, VisibleColumns } from "./ThreeColumnLayout.types";
